0bd51ec1b360e4ef4df2f7e7cb783259848ae125
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-04-29 12:01:34 +0530

    update booking

--- database/booking-prisma-data-store.ts
+++ database/booking-prisma-data-store.ts
@@ -15,6 +15,7 @@ export async function addBooking(b:Booking){
     console.log("booking added successfully",newBooking);
     return newBooking;
 }
+
 export async function getAllBookings(){
     try {
         return await prisma.booking.findMany();
@@ -32,7 +33,25 @@ export async function DeleteBooking(id:string){
         return deleteBooking
     }catch (err){
         console.log("Booking delete Unsuccessfully",err)
+    }
+}
 
+export async function UpdateBooking(id:string,b:Booking){
+    try {
+        const updateBooking=await prisma.booking.update({
+            where:{id:id},
+            data:{
+               customerName:b.customerName,
+               address:b.address,
+                dateTime:b.dateTime,
+                serviceId:b.serviceId,
+                userId:b.userId
+            }
+        })
+        console.log("Booking update successfully")
+        return updateBooking
+    }catch (err){
+        console.log("Booking Update unsuccessfully",err)
     }
 }
 
--- routes/Booking-routes.ts
+++ routes/Booking-routes.ts
@@ -1,9 +1,7 @@
 import express from 'express';
-import {addBooking, DeleteBooking, getAllBookings} from "../database/booking-prisma-data-store";
+import {addBooking, DeleteBooking, getAllBookings, UpdateBooking} from "../database/booking-prisma-data-store";
 import Booking from "../model/Booking";
 const router = express.Router();
-
-
 
 router.post('/add', async (req, res) => {
     const body = req.body;
@@ -12,10 +10,8 @@ router.post('/add', async (req, res) => {
         const addBookings = await addBooking(booking);
         res.json(addBookings)
     }catch (e){
-
         res.status(400).send("Error Adding Books")
     }
-
 });
 router.get('/view',async (req,res)=>{
     try {
@@ -24,7 +20,6 @@ router.get('/view',async (req,res)=>{
     }catch (err){
         console.log("error getting bookings",err)
     }
-
 })
 router.delete('/delete/:id',async (req,res)=>{
     const id: string  = req.params.id;
@@ -35,5 +30,15 @@ router.delete('/delete/:id',async (req,res)=>{
         console.log("Error deleting Booking",err)
     }
 })
-
+router.put('/update/:id',async (req,res)=>{
+    const booking:Booking=req.body;
+    const id: string  = req.params.id;
+    try {
+        const updateBooking=await UpdateBooking(id,booking);
+        res.send(updateBooking)
+    }catch (err){
+        console.log("Error updating Booking",err)
+        res.status(400)
+    }
+})
 export default router;
